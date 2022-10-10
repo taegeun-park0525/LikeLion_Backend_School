6857dfa18fcba6d262547c8aea462ee6e8383258
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-11 04:19:51 +0900

    데이터처리하기 연습
    데이터 읽어오기 성공
    데이터 쓰기 성공
    읽어온 데이터 원하는 값만 출력하기 성공

--- src/Week3/Fri/LargeDataProcessing/DoSomething.java
+++
@@ -1,7 +0,0 @@
-package Week3.Fri.LargeDataProcessing;
-
-import java.util.List;
-
-public interface DoSomething<T> {
-    List<T> work(String str);
-}
--- src/Week3/Fri/LargeDataProcessing/PopulationMove.java
+++ src/Week3/Fri/LargeDataProcessing/PopulationMove.java
@@ -1,23 +1,37 @@
 package Week3.Fri.LargeDataProcessing;
 
+import java.util.ArrayList;
+
 public class PopulationMove {
+    private int outSido;    //전출
+    private int comeSido;   //전입
 
-    private int fromSido;
-    private int toSido;
-
-    // constructor 추가 fromSido, toSido를 받아서 멤버 변수에 넣는 기능
-    public PopulationMove(int fromSido, int toSido) {
-        this.fromSido = fromSido;
-        this.toSido = toSido;
+    public PopulationMove(){}; // 기본생성자
+    public PopulationMove(int outsido, int comeSido) { //전입 전출 코드를 받아 저장하는 생성자
+        this.comeSido = comeSido;
+        this.outSido = outsido;
+    }
+//게터 추가
+    public int getOutSido() {
+        return outSido;
     }
 
-    public int getFromSido() {
-        return fromSido;
+    public int getComeSido() {
+        return comeSido;
+    }
+//세터추가
+    public void setOutSido(int outSido) {
+        this.outSido = outSido;
     }
 
-    public int getToSido() {
-        return toSido;
+    public void setComeSido(int comeSido) {
+        this.comeSido = comeSido;
     }
 
-
+    public PopulationMove parse(String data) { //문자열을 나누어 String[]을 만듦
+        String dividedData[] = data.split(",");
+        System.out.println(dividedData[0]);
+        System.out.println(dividedData[6]);
+        return new PopulationMove(Integer.parseInt(dividedData[0]), Integer.parseInt(dividedData[6]));
+    }
 }
