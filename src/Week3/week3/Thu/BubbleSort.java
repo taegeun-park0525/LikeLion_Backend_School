cbf8855ad45b85436f2ffd2799ad1f83f0a530ff
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-13 09:47:03 +0900

    Bubble Sort 배열명 수정
    a-> bubbleArr

--- src/Week3/week3/Thu/BubbleSort.java
+++ src/Week3/week3/Thu/BubbleSort.java
@@ -3,20 +3,20 @@ package Week3.week3.Thu;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] a = {9,25,12,3,4,7,5,6,8,11,12,35};
+        int[] bubbleArr = {9,25,12,3,4,7,5,6,8,11,12,35};
         int tmp;
-        for(int i = 0 ; i < a.length ; i ++) {
-            for(int j = 0 ; j < a.length -i -1 ; j ++) {
-                if(a[j]>a[j+1]) {
-                    tmp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = tmp;
+        for(int i = 0 ; i < bubbleArr.length ; i ++) {
+            for(int j = 0 ; j < bubbleArr.length -i -1 ; j ++) {
+                if(bubbleArr[j]>bubbleArr[j+1]) {
+                    tmp = bubbleArr[j];
+                    bubbleArr[j] = bubbleArr[j+1];
+                    bubbleArr[j+1] = tmp;
                 }
             }
         }
 
-        for(int i = 0 ; i < a.length ; i ++) {
-            System.out.print(a[i]+ " ");
+        for(int i = 0 ; i < bubbleArr.length ; i ++) {
+            System.out.print(bubbleArr[i]+ " ");
         }
 
     }
