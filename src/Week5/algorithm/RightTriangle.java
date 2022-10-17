4355cda31e77edd3488d375f86ae9b1381052454
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-17 09:10:05 +0900

    별찍기 직각삼각형
    *
    **
    ***
    ****

---
+++ src/Week5/algorithm/RightTriangle.java
@@ -0,0 +1,12 @@
+package Week5.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
