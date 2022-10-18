c0d3890cd73843a1c67f262ee5738526e1b9f621
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-18 09:31:47 +0900

    [알고리즘] 사각형 도넛 별
    *****
    *     *
    *     *
    *     *
    *****

--- src/Week5/Tue/Algorithm/Main.java
+++ src/Week5/Tue/Algorithm/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         SquareStar ss = new SquareStar();
         RectangleStar rs = new RectangleStar();
+        DonutSquare ds = new DonutSquare();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         //ss.printSquareMatrix(n);
-        rs.printRectangleMatrix(n,m);
+        //rs.printRectangleMatrix(n,m);
+        ds.printDonutSquare(n);
     }
 }
