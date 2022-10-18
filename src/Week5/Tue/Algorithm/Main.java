d1316ddce1d8e7b187de96f5db791a89914fe3cc
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-18 09:22:15 +0900

    [알고리즘] 직사각형 별찍기
    .repeat을 이용해 출력하기

--- src/Week5/Tue/Algorithm/Main.java
+++ src/Week5/Tue/Algorithm/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         SquareStar ss = new SquareStar();
+        RectangleStar rs = new RectangleStar();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        ss.printSquareMatrix(n);
-
+        int m = sc.nextInt();
+        //ss.printSquareMatrix(n);
+        rs.printRectangleMatrix(n,m);
     }
 }
