cadabd8c102767c35d2bc77be760ccab105e652d
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-18 09:50:47 +0900

    [알고리즘] 평행사변형 별

--- src/Week5/Tue/Algorithm/Main.java
+++ src/Week5/Tue/Algorithm/Main.java
@@ -7,11 +7,14 @@ public class Main {
         SquareStar ss = new SquareStar();
         RectangleStar rs = new RectangleStar();
         DonutSquare ds = new DonutSquare();
+        ParallelogramStar ps = new ParallelogramStar();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        //int m = sc.nextInt();
         //ss.printSquareMatrix(n);
         //rs.printRectangleMatrix(n,m);
-        ds.printDonutSquare(n);
+        //ds.printDonutSquare(n);
+        ps.printParallelogramStar(n);
     }
 }
---
+++ src/Week5/Tue/Algorithm/ParallelogramStar.java
@@ -0,0 +1,14 @@
+package Week5.Tue.Algorithm;
+
+public class ParallelogramStar {
+    public void printParallelogramStar(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n-i; j++) {
+                System.out.printf(" ");
+            }
+            System.out.printf("*".repeat(n));
+            System.out.println("");
+        }
+    }
+
+}
