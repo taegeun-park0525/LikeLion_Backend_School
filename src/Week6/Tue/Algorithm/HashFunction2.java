ba08a435bdbab0d54c46b0fe91bf047f81a5ce5c
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-25 09:23:35 +0900

    [Hash]

--- src/Week6/Tue/Algorithm/HashFunction2.java
+++ src/Week6/Tue/Algorithm/HashFunction2.java
@@ -1,2 +1,16 @@
-package Week6.Tue.Algorithm;public class HashFunction2 {
+package Week6.Tue.Algorithm;
+
+public class HashFunction2 {
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % 90;
+    }
+
+    public static void main(String[] args) {
+        HashFunction2 hf = new HashFunction2();
+        hf.hash("Taegeun");
+    }
 }
--- src/Week6/Tue/Algorithm/HashTable.java
+++ src/Week6/Tue/Algorithm/HashTable.java
@@ -1,2 +1,19 @@
-package Week6.Tue.Algorithm;public class HashTable {
+package Week6.Tue.Algorithm;
+
+public class HashTable {
+    int size = 10000;
+
+    public HashTable() {}
+    public HashTable(int size) {
+        this.size = size;
+    }
+
+    public int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % size;
+    }
+
 }
