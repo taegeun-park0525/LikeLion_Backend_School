eb6cbc0e03323ea0d7cbeda04c82669cf3b90d4e
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-26 10:52:46 +0900

    [22.10.26] Hash 알고리즘

---
+++ src/Week6/Wed/Algorithm/HashTable3.java
@@ -0,0 +1,72 @@
+package Week6.Wed.Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable3 {
+    class Node {
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
+    List<Node>[] table = new ArrayList[1000];
+    public int hash(String str) {
+        int ascii = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ascii += str.charAt(i);
+        }
+        return ascii % 1000;
+    }
+
+    public void insert(String key, int value) {
+        // List
+        int hashIdx = hash(key);
+        if (this.table[hashIdx] == null) {
+            this.table[hashIdx] = new ArrayList<>();
+        }
+        // Map, Object
+        this.table[hashIdx].add(new Node(key, value));
+    }
+
+    public Integer get(String key) {
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node : nodes) {
+            if (key.equals(node.getKey())) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashTable3 ht = new HashTable3();
+        ht.insert("Yoonseo", 1);
+        ht.insert("Seoyoon", 2);
+
+        int result = ht.get("Yoonseo");
+        if (result == 1) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
+
+        result = ht.get("Seoyoon");
+        if (result == 2) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
+
+    }
+}
