e62280b48be362baf0de8c785e6404c09374520e
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-07 10:30:41 +0900

    나누어 떨어지는 숫자 배열 PriorityQueue를 사용하는 풀이 추가

--- src/week8/mon/CanDivide.java
+++ src/week8/mon/CanDivide.java
@@ -3,6 +3,7 @@ package week8.mon;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class CanDivide {
     public List<Integer> solution(int[] arr, int divisor) {
@@ -18,10 +19,27 @@ public class CanDivide {
         return answer;
     }
 
+    public int[] solution2(int[] arr, int divisor) {
+        PriorityQueue<Integer> list = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i ++) {
+            if (arr[i] % divisor == 0) list.add(arr[i]);
+        }
+
+        if(list.size()==0) return  new int[]{-1};
+
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        while (!list.isEmpty()){
+            answer[idx++] = list.poll();
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         CanDivide canDivisor = new CanDivide();
         int[] arr = {5,9,7,10};
         int divisor = 5;
         canDivisor.solution(arr, divisor);
+        System.out.println(Arrays.toString(canDivisor.solution2(arr,divisor)));
     }
 }
