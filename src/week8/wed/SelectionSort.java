fd83763398cf54cf2cc1ef69cd6a8ec2d9386b65
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-09 11:06:11 +0900

    BiFunction 적용

--- src/week8/wed/SelectionSort.java
+++ src/week8/wed/SelectionSort.java
@@ -1,29 +1,30 @@
 package week8.wed;
 
-public class SelectionSort {
-    private static int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3 ,39};
+import java.util.Arrays;
+import java.util.function.BiFunction;
 
-    public static void swap(int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+public class SelectionSort {
+    public int[] SelectionSort(int[] arr , BiFunction<Integer, Integer, Boolean> stmt) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (stmt.apply(arr[minIdx], arr[j])) minIdx = j; // stmt 의 매개변수
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+            System.out.println(Arrays.toString(arr));
+
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int swapIdx = i;
-            int min = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (min > arr[j]) {
-                    min = arr[j];
-                    swapIdx = j;
-                }
-            }
-            swap(i, swapIdx);
-        }
-
-        for (int i : arr) {
-            System.out.printf("%d ", i);
-        }
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort ss = new SelectionSort();
+        ss.SelectionSort(arr, (a, b) -> a < b);
+        ss.SelectionSort(arr, (a, b) -> a > b);
+        BiFunction<Integer, Integer, Boolean> biFunction = (a, b) -> a > b;
     }
 }
