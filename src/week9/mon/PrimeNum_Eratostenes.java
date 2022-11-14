737430836893635f45b1a67c6827b1a764179ae9
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-14 09:55:27 +0900

    에라토스태네스의 채 2를 제외한 2의 배수 0으로 만들기

--- src/week9/mon/PrimeNum_Eratostenes.java
+++ src/week9/mon/PrimeNum_Eratostenes.java
@@ -1,19 +1,29 @@
 package week9.mon;
 
+import java.util.Arrays;
+
 public class PrimeNum_Eratostenes {
     public int[] Eratos(int[] arr) {
+
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] % 2 == 0)
+            if (arr[i] == 2) {
+            } else if (arr[i] % 2 == 0) {
+                arr[i] = 0;
+            }
         }
+        return arr;
     }
+
 
     public static void main(String[] args) {
         PrimeNum_Eratostenes primeNum_eratostenes = new PrimeNum_Eratostenes();
         int[] arr = new int[50];
         for (int i = 2; i <= 50; i++) {
-            arr[i-2]= i;
+            arr[i - 2] = i;
         }
 
         primeNum_eratostenes.Eratos(arr);
+
+        System.out.println(Arrays.toString(arr));
     }
 }
