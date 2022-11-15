4418e1c6d2f9bf3be1c7e4b01356128d71371433
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-15 09:16:03 +0900

    에라토스테네스 채

--- src/week9/mon/PrimeNum_Eratostenes.java
+++ src/week9/mon/PrimeNum_Eratostenes.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class PrimeNum_Eratostenes {
     public int[] Eratos(int[] arr) {
-
+        int num = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 2) {
             } else if (arr[i] % 2 == 0) {
@@ -17,7 +17,8 @@ public class PrimeNum_Eratostenes {
 
     public static void main(String[] args) {
         PrimeNum_Eratostenes primeNum_eratostenes = new PrimeNum_Eratostenes();
-        int[] arr = new int[50];
+
+        int[] arr = new int[50-1];
         for (int i = 2; i <= 50; i++) {
             arr[i - 2] = i;
         }
