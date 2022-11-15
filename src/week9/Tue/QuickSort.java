ad6f91a5a865dab6ece0eb7a56850a358f792959
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-15 10:07:25 +0900

    Quick Sort 기준값 뽑는 로직, 기준값 기준으로 오른쪽 왼쪽나누는 로직 구현

--- src/week9/Tue/QuickSort.java
+++ src/week9/Tue/QuickSort.java
@@ -1,4 +1,23 @@
 package week9.Tue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
+
+        int pivot = arr[arr.length / 2];
+        System.out.println(pivot);
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (pivot < arr[i]) right.add(arr[i]);
+            else if (pivot < arr[i]) right.add(arr[1]);
+        }
+        System.out.println(left);
+        System.out.println(right);
+    }
 }
