857904135070711d9250ab95210308003266abea
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-16 09:43:24 +0900

    [add] leftidx가 4까지

--- src/week9/wed/QuickSortArray.java
+++ src/week9/wed/QuickSortArray.java
@@ -1,4 +1,28 @@
 package week9.wed;
 
+import week8.thu.Solution;
+
+import java.util.Arrays;
+
 public class QuickSortArray {
+
+    public int[] solution(int[] arr) {
+        int leftidx = 0;
+        int rightidx = arr.length;
+        int pivot = arr[arr.length / 2];
+
+        while(arr[leftidx] < pivot) {
+            leftidx++;
+            System.out.println(arr[leftidx]);
+        }
+
+
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        QuickSortArray QA = new QuickSortArray();
+        int[] arr = {20,18,5,19,40,50,5,25};
+        QA.solution(arr);
+    }
 }
