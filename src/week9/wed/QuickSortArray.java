b967c5c17584922e9371595ca099b65d0fa94faf
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-17 10:17:22 +0900

    [update] QuickSort 완성

--- src/week9/wed/QuickSortArray.java
+++ src/week9/wed/QuickSortArray.java
@@ -6,23 +6,42 @@ import java.util.Arrays;
 
 public class QuickSortArray {
 
-    public int[] solution(int[] arr) {
-        int leftidx = 0;
-        int rightidx = arr.length;
-        int pivot = arr[arr.length / 2];
+    public int[] sort(int[] arr, int startIdx, int endIdx) {
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
+        int pivot = arr[(startIdx + endIdx) / 2];
+        // 언제까지 반복되는지? leftIdx == rightIdx일 때도 아래 로직이 반복됨
+        // ex) leftIdx = 3, rightIdx = 3일 때도
+        // 왼쪽과 오른쪽 각각 교환할 index를 정하는 부분 pivot까지 올 수도 있다.
+        while (leftIdx <= rightIdx) {
+            while (arr[leftIdx] < pivot) leftIdx += 1;   // <=아닙니다.
+            while (arr[rightIdx] > pivot) rightIdx -= 1;   // <=아닙니다.
+            // leftIdx = 4, rightIdx = 7 그대로 왜 냐하면 25는 교환 대상이기 때문입니다.
 
-        while(arr[leftidx] < pivot) {
-            leftidx++;
-            System.out.println(arr[leftidx]);
+            if (leftIdx <= rightIdx) {
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                leftIdx += 1;
+                rightIdx -= 1;
+            }
+            System.out.printf("leftIdx:%d rightIdx:%d\n", leftIdx, rightIdx);
+            System.out.println(Arrays.toString(arr));
         }
+        // 교환이 모두 끝나면 왼쪽, 오른쪽 두 그룹으로 나누어 지면 됩니다.
+        // 20, 18, 5, 19, 25, 5, 50, 40
 
-
+        if (startIdx < rightIdx) sort(arr, startIdx, rightIdx);
+        if (leftIdx < endIdx) sort(arr, leftIdx, endIdx);
 
         return arr;
     }
+
     public static void main(String[] args) {
-        QuickSortArray QA = new QuickSortArray();
-        int[] arr = {20,18,5,19,40,50,5,25};
-        QA.solution(arr);
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        QuickSortArray quickSortArray = new QuickSortArray();
+        var r = quickSortArray.sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(r));
     }
 }
+
