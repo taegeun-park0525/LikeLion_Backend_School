0a123a132323cd90be974ecc25c9984d18e5a888
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-18 09:41:55 +0900

    [add] 시저암호 알고리즘 문제 ~ing
    String으로 입력받아 Charat오로 변환시켜 +1해주는 것까지 성공

--- src/week9/fri/CaesarCipher.java
+++ src/week9/fri/CaesarCipher.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class CaesarCipher {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println((char)(str.charAt(i)+1));
+        }
 
     }
 }
