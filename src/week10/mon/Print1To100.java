63465c7f36d41fb5d08b5a592cea19370e82f5f4
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-22 09:16:49 +0900

    [update] 재귀함수 Print1To100 완성

--- src/week10/mon/Print1To100.java
+++ src/week10/mon/Print1To100.java
@@ -5,10 +5,15 @@ import week8.thu.Solution;
 public class Print1To100 {
     public static void print1To100(int n) {
         System.out.println(n);
+        n++;
+        if(n==101) {
+            return;
+        }
         print1To100(n);
     }
 
     public static void main(String[] args) {
         print1To100(1);
+        System.out.println("1에서 100까지 끝!");
     }
 }
