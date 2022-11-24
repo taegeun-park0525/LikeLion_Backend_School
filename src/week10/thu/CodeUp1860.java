dfb79831ace0289b61948c3055a5f817eec744f9
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-24 09:45:02 +0900

    [update] 수 삼각형 출력하기 작동은함
    띄어쓰기 출력
    파라미터를 줄여보는 방법을 생각해보자

--- src/week10/thu/CodeUp1860.java
+++ src/week10/thu/CodeUp1860.java
@@ -6,13 +6,13 @@ public class CodeUp1860 {
 
     public static int printNum(int num, int count, int numCount) {
         if(numCount == count) {
-            System.out.println(numCount);
+            System.out.println(numCount + " ");
             count ++;
             if (num < count) return 0;
             numCount = 1;
             return printNum(num,count,numCount);
         }
-        System.out.print(numCount);
+        System.out.print(numCount+ " ");
         numCount ++;
         return printNum(num, count, numCount);
     }
