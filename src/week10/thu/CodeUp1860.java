27dd7ee56b3415d6880ea1c64c9edaf4ea2c4bcf
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-24 09:43:57 +0900

    [add] 수 삼각형 출력하기 작동은함
    파라미터를 줄여보는 방법을 생각해보자

---
+++ src/week10/thu/CodeUp1860.java
@@ -0,0 +1,27 @@
+package week10.thu;
+
+import java.util.Scanner;
+
+public class CodeUp1860 {
+
+    public static int printNum(int num, int count, int numCount) {
+        if(numCount == count) {
+            System.out.println(numCount);
+            count ++;
+            if (num < count) return 0;
+            numCount = 1;
+            return printNum(num,count,numCount);
+        }
+        System.out.print(numCount);
+        numCount ++;
+        return printNum(num, count, numCount);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 1;
+        int numCount = 1;
+        printNum(num, count, numCount);
+    }
+}
