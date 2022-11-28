ae0f93ed86dcd06c36dd432ac1a1fc41743a204a
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-28 09:22:23 +0900

    [add] 알파뱃 AA ~ ZZ까지 출력

--- src/week11/AlphabetCombination.java
+++ src/week11/AlphabetCombination.java
@@ -10,8 +10,20 @@ public class AlphabetCombination {
             System.out.print(alphabat[i]);
         }
     }
+
+    public static void twoAlpha(){
+        char[] alphabat = new char[26];
+        for(int i = 0; i<alphabat.length; i++){
+            alphabat[i] =(char)('A' + i);
+            for (int j = 0; j < alphabat.length; j++) {
+                System.out.print(alphabat[i]);
+                System.out.println(alphabat[j]);
+            }
+        }
+
+    }
     public static void main(String[] args) {
-        oneAlpha();
+        twoAlpha();
 
     }
 }
