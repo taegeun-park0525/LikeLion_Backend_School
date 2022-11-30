1b6e77f729763fb453ba5609794ee62b9922c58d
taegeun <djqtek0525@naver.com>
2022-11-30 09:39:16 +0900

    [add] 1936 두 노드간의 거리 완성

--- src/week11/wed/CodeUp1936.java
+++ src/week11/wed/CodeUp1936.java
@@ -7,10 +7,26 @@ public class CodeUp1936 {
     부모가 N이면 자식은 2N+1, 2N+2임
     a,b 각각의 부모가 될수있는 노드들을 배열에 저장함
     비교해서 같은부모를 찾음
-    각각의 인덱스를를 이용해 거리를 구해준다??
+    각각의 인덱스를를 이용해 거리를 구해준다?? -> 너무 많이 자원소비할듯
+    자식을 2로나누어줌 => 부모가됨
+    ab 크기를 비교해서 나누고 그걸 비교하는 로직을 만든다
      */
+    public static int findParents(int a, int b) {
+        if(a == b) {
+            return 0;
+        }else if(a < b) {
+            return findParents(a, b/2) +1;
+        }else if(a > b) {
+            return findParents(a/2, b)+1;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
+    int a = 3;
+    int b = 4;
+
+        System.out.println(findParents(a,b));
 
     }
 }
