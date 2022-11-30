bec9951b9953b112fec1ddaf9a95eee4346e82a6
taegeun <djqtek0525@naver.com>
2022-11-30 09:22:53 +0900

    [add] 1936 두 노드간의 거리

---
+++ src/week11/wed/CodeUp1936.java
@@ -0,0 +1,16 @@
+package week11.wed;
+
+public class CodeUp1936 {
+    /*
+    공통된 부모를 찾을때까지 올라가야함
+    올라가면서 count + 1을 해주며 거리를 계산
+    부모가 N이면 자식은 2N+1, 2N+2임
+    a,b 각각의 부모가 될수있는 노드들을 배열에 저장함
+    비교해서 같은부모를 찾음
+    각각의 인덱스를를 이용해 거리를 구해준다??
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
