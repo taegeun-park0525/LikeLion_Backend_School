abcbeca384017091e814fd5909c6ebad3f8c7a5d
taegeun <djqtek0525@naver.com>
2022-11-30 10:25:31 +0900

    [add] 문자열 내p,y개수

--- src/week11/wed/Programmers12916.java
+++ src/week11/wed/Programmers12916.java
@@ -1,5 +1,6 @@
 package week11.wed;
 
+import java.util.List;
 import java.util.Stack;
 
 public class Programmers12916 {
@@ -16,11 +17,26 @@ public class Programmers12916 {
         return stack.empty();
     }
 
+    public static boolean solution2(String s) {
+        s.toLowerCase();
+        int countP = 0;
+        int countY = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'p'){
+                countP ++;
+            } else if (s.charAt(i)=='y') {
+                countY ++;
+            }
+        }
+        return countP == countY;
+    }
+
     public static void main(String[] args) {
         String s1 = "pPoooyY"; //true
         String s2 = "Pyy"; //false
-        System.out.println(solution(s1));
-        System.out.println(solution(s2));
+        System.out.println(solution2(s1));
+        System.out.println(solution2(s2));
 
     }
 }
